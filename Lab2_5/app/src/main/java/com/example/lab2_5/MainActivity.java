05e5e25ce9e31b75cc84a6343071b227337f6519
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2023-11-15 12:28:42 +0700

    Update

--- Lab2_5/app/src/main/java/com/example/lab2_5/MainActivity.java
+++ Lab2_5/app/src/main/java/com/example/lab2_5/MainActivity.java
@@ -8,15 +8,20 @@ import static com.example.lab2_5.Thumbnail.THUMBNAIL_4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    String selectedItemText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -38,14 +43,30 @@ public class MainActivity extends AppCompatActivity
             add(THUMBNAIL_4);
         }};
         Spinner spinner = findViewById(R.id.spinner);
-        CustomSpinnerAdapter spinnerAdaper= new CustomSpinnerAdapter(this, thumbnails);
+        CustomSpinnerAdapter spinnerAdaper = new CustomSpinnerAdapter(this, thumbnails);
         spinner.setAdapter(spinnerAdaper);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                // Retrieve the selected item
+                selectedItemText = ((TextView) view.findViewById(R.id.tvSpinner)).getText().toString();
+                // Do something with the selected item
+                //Toast.makeText(null, selectedItemText, Toast.LENGTH_SHORT);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
 
+            }
+        });
+        EditText editText = findViewById(R.id.etName);
         Button btAdd = findViewById(R.id.btAddDish);
         btAdd.setOnClickListener((v ->
         {
-            Dish dish = new Dish("123", THUMBNAIL_1);
+            Dish dish = new Dish(editText.getText().toString(), thumbnails[]);
             dishes.add(dish);
             adapter.notifyDataSetChanged();
         }));
